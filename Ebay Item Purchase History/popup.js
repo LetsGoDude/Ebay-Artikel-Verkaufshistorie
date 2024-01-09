f4c8a0ec76ce4f47eb8879fed6ea1f476d26d20e
LetsGoDude <paulkrapp94@gmx.de>
2024-01-09 13:16:10 +0100

    Delete Ebay Item Purchase History directory

--- Ebay Item Purchase History/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var historyButton = document.getElementById("historyButton");
-
-  historyButton.addEventListener("click", function () {
-    // Hier die URL zur Anzeige der Bestellhistorie einfügen
-    var historyUrl = "https://example.com/order-history";
-    browser.tabs.create({ url: historyUrl });
-  });
-});
